4b76de26c30bf293820af91fd2b8325b8fad47ca
AlexisGalarza <galarzaalexis19@gmail.com>
2024-06-12 14:47:24 -0300

    Agregar archivos de proyecto.

---
+++ Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Dim num, contador, suma, promedio As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        num = TextBox1.Text
+        If num <> 0 Then
+            contador += 1
+            suma += num
+            promedio = suma / contador
+            TextBox2.Text = promedio
+            TextBox1.Clear()
+        Else
+            Button1.Enabled = False
+        End If
+    End Sub
+End Class
